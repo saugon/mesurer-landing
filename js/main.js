b21af87396487be4ebba04691222d2ff459340de
Gonza <gonzalo.torres@lambdaclass.com.ar>
2021-04-19 13:59:14 -0300

    Added menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let scrollPos = -10000;
+
   new Glide("#team", {
     type: "carousel",
     perView: 3,
@@ -11,4 +13,33 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   }).mount();
+
+  window.addEventListener("scroll", function () {
+    if (document.body.getBoundingClientRect().top < scrollPos && document.body.getBoundingClientRect().top < -30) {
+      document.getElementsByTagName("header")[0].classList.add("scroll-hide");
+    } else {
+      document.getElementsByTagName("header")[0].classList.remove("scroll-hide");
+    }
+    scrollPos = document.body.getBoundingClientRect().top;
+  });
+
+  const menuTrigger = document.getElementById("menu-trigger");
+  menuTrigger.addEventListener("click", () => {
+    document.getElementById("menu").classList.add("show");
+  });
+
+  const closeMenu = document.getElementById("close-menu");
+  closeMenu.addEventListener("click", () => {
+    document.getElementById("menu").classList.remove("show");
+  });
+
+  const menuLinks = document.querySelectorAll("#menu .links a");
+  menuLinks.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      document.getElementById("menu").classList.remove("show");
+      setTimeout(() => {
+        document.getElementsByTagName("header")[0].classList.add("scroll-hide");
+      }, 100);
+    });
+  });
 });
